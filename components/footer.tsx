adef97b9c6e360f443c6e7515b06f4f4bde8b188
Rami Maalouf <rami.rami@ucalgary.ca>
2023-01-28 23:47:10 -0500

    centered made w love and added hover animation

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,27 +5,44 @@ import { BiHomeAlt } from "react-icons/bi";
 import { AiFillYoutube } from "react-icons/ai";
 import { HStack } from "@chakra-ui/react";
 import {color} from "../lib/visualColor";
+import { useState } from "react";
 
 
 export default function Footer(props: { renderForm: string}) {
   const { renderForm } = props;
-  var visColor = renderForm === "insert" ? color.insert
-        : renderForm === "delete" ? color.delete
-            : renderForm === "search" ? color.search
-            : renderForm === "increment" ? color.increment
-            : renderForm === "reset" ? color.reset
-            : color.default;
-  // console.log(`text-[${visColor}]`);
+  var visColor = color.default;
+  const [textHover, setTextHover] = useState(false);
+  const [nameHover, setNameHover] = useState(false);
+  
+
+    visColor = renderForm === "insert" ? color.insert
+          : renderForm === "delete" ? color.delete
+              : renderForm === "search" ? color.search
+              : renderForm === "increment" ? color.increment
+              : renderForm === "reset" ? color.reset
+              : color.default;
+
   
   return (
       <section className="bottom-0">
-      <p className="text-right pb-4 mr-8">
+      <p 
+      onMouseEnter={() => setTextHover(true)}
+      onMouseLeave={() => setTextHover(false)}
+      className="text-center pb-4">
         Made with ❤️ by{" "}
         <a
+        onMouseEnter={() => setNameHover(true)}
+        onMouseLeave={() => setNameHover(false)}
+        style={{
+          color: textHover ? "red" : visColor,
+          textDecoration: nameHover ? "underline" : "none",
+          transition: "all 0.3s ease-in-out",
+          animation: "pulse 1s infinite",
+          
+        }}
           href="https://rami-maalouf.vercel.app/"
           target={"_blank"}
           rel={"noopener noreferrer"}
-          className={`text-[${visColor}] text-lime-300 hover:text-orange-300`}
         >
           Rami
         </a>
